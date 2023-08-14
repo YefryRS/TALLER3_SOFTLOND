173d4dfe641eabd819281ca3194a16576fe78f39
yefry <yefryrodelo@gmail.com>
2023-08-14 15:05:06 -0500

    finalizado

--- Ejercicio4/src/Main.java
+++ Ejercicio4/src/Main.java
@@ -3,11 +3,7 @@ import java.util.List;
 
 public class Main {
     /* 4. Juego de cartas: Crea un juego de cartas en el que puedas representar diferentes tipos de cartas (por ejemplo, cartas de póker) utilizando una jerarquía de clases y una interfaz. La interfaz podría definir métodos como mostrarCarta y valorNumerico. Luego, implementa clases para los diferentes tipos de cartas y para la baraja. */
-
-    // clase abstracta carta
-    // implementar cartas nipes, poker, UNO
-    // poker tiene una lista de cartas, cada carta un nombre y un puntaje.
-    // Si no necesitamos la baraja no la implementamos
+    
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
         baraja.barajar();
